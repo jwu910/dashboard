9be92c549b1e8517785fb15503a3c50ca75f4ffa
Ryan Garant <ryantgarant@gmail.com>
2019-02-19 14:10:19 -0800

    add proxy server

---
+++ proxy.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const { Base64 } = require('js-base64')
+const app = require('express')()
+const axios = require('axios')
+
+const credential = Base64.encode(
+	`${process.env.JIRA_USER}:${process.env.JIRA_PASS}`
+)
+
+app.get('/api/jira', (req, res) => {
+	try {
+		axios
+			.get(`https://issues.liferay.com/rest/api/${req.query.query}`, {
+				headers: { Authorization: `Basic ${credential}` },
+			})
+			.then(({ data }) => res.json(data))
+			.catch(err => console.log('err', err))
+	} catch (error) {
+		console.error(`Error ===>`)
+	}
+})
+
+app.listen(3001, () => console.log('Proxy server listening on 3001'))
